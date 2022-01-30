0d8dc9e8dd6de4ab980076b47811f68c7660cbab
RoyTakanen <roy@imap.fi>
2022-01-30 14:14:57 +0200

    fix about page visibility

--- generate.js
+++ generate.js
@@ -11,12 +11,17 @@ converter = new showdown.Converter();
 fs.rmSync('./docs', { recursive: true, force: true });
 fs.mkdirSync('./docs');
 
+if (about.exists()) {
+    const about = true;
+}
+
 const info = JSON.parse(fs.readFileSync('./content/info.json'));
 
 // About page
 const aboutHtml = pug.renderFile('./views/about.pug', {
     info,
-    content: converter.makeHtml(about.read())
+    content: converter.makeHtml(about.read()),
+    about
 });
 
 fs.writeFileSync('./docs/about.html', aboutHtml);
@@ -25,7 +30,8 @@ fs.writeFileSync('./docs/about.html', aboutHtml);
 if (frontpage.exists()) {
     const frontpageHtml = pug.renderFile('./views/index.pug', {
         info,
-        content: converter.makeHtml(frontpage.read())
+        content: converter.makeHtml(frontpage.read()),
+        about
     });
     fs.writeFileSync('./docs/index.html', frontpageHtml);
 } else {
@@ -39,7 +45,8 @@ if (frontpage.exists()) {
 // Blog
 const blogHtml = pug.renderFile('./views/list.pug', {
     info,
-    articles: articles.list()
+    articles: articles.list(),
+    about
 });
 
 fs.writeFileSync('./docs/blog.html', blogHtml);
@@ -52,7 +59,8 @@ articles.list().forEach((article) => {
     const articleHtml = pug.renderFile('./views/article.pug', {
         info,
         content: articles.read(article.name, 0, true),
-        title: article.name
+        title: article.name,
+        about
     });
 
     fs.writeFileSync(`./docs/${article.name}.html`, articleHtml);
